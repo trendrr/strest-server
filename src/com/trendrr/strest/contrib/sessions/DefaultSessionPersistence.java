e61c1f344b3f169dfe54ed1a62c29eecaeca7f59
dustin norlander <dustinn@gmail.com>
2011-06-14 16:50:11 -0700

    Start on sessions

---
+++ src/com/trendrr/strest/contrib/sessions/DefaultSessionPersistence.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.trendrr.strest.contrib.sessions;
+
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.trendrr.oss.DynMap;
+
+
+/**
+ * @author Dustin Norlander
+ * @created Jun 14, 2011
+ * 
+ */
+public class DefaultSessionPersistence implements SessionPersistence {
+
+	protected Log log = LogFactory.getLog(DefaultSessionPersistence.class);
+
+	/* (non-Javadoc)
+	 * @see com.trendrr.strest.contrib.sessions.SessionPersistence#loadSession(java.lang.String)
+	 */
+	@Override
+	public Map<String, Object> loadSession(String sessionId) {
+		log.info("Loading session (not really): " + sessionId);
+		return new DynMap();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.trendrr.strest.contrib.sessions.SessionPersistence#saveSession(java.lang.String, java.util.Map)
+	 */
+	@Override
+	public void saveSession(String sessionId, Map<String, Object> values) {
+		log.info("Saving session (not really though) : " + sessionId + " " + values);
+	}
+}
---
+++ src/com/trendrr/strest/contrib/sessions/SessionFilter.java
@@ -0,0 +1,135 @@
+/**
+ * 
+ */
+package com.trendrr.strest.contrib.sessions;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.netty.handler.codec.http.Cookie;
+import org.jboss.netty.handler.codec.http.CookieDecoder;
+import org.jboss.netty.handler.codec.http.CookieEncoder;
+import org.jboss.netty.handler.codec.http.DefaultCookie;
+import org.jboss.netty.handler.codec.http.HttpHeaders;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.util.internal.ConcurrentHashMap;
+
+import com.trendrr.oss.IsoDateUtil;
+import com.trendrr.oss.Reflection;
+import com.trendrr.strest.StrestException;
+import com.trendrr.strest.server.StrestController;
+import com.trendrr.strest.server.StrestControllerFilter;
+
+
+/**
+ * @author Dustin Norlander
+ * @created Jun 14, 2011
+ * 
+ */
+public class SessionFilter implements StrestControllerFilter {
+
+	protected Log log = LogFactory.getLog(SessionFilter.class);
+	protected static String SESSION = "sessionId";
+	protected int maxAge = 60*30; //30 minutes
+	
+	protected ConcurrentHashMap<String, SessionPersistence> persistence = new ConcurrentHashMap<String,SessionPersistence>();
+	
+	/* (non-Javadoc)
+	 * @see com.trendrr.strest.server.StrestControllerFilter#before(com.trendrr.strest.server.StrestController)
+	 */
+	@Override
+	public void before(StrestController controller) throws StrestException {
+		if (controller.isStrest()) {
+			//sessions unnecessary
+			return;
+		}
+        
+		String sessionId = null;
+		 // get the session cookie.
+        String cookieString = controller.getRequest().getHeader(HttpHeaders.Names.COOKIE);
+        if (cookieString != null) {
+            CookieDecoder cookieDecoder = new CookieDecoder();
+            Set<Cookie> cookies = cookieDecoder.decode(cookieString);
+            for (Cookie cookie : cookies) {
+            	if (cookie.getName().equals(SESSION)) {
+            		sessionId = cookie.getValue();
+            	}
+            }
+            if(!cookies.isEmpty()) {
+                // Reset the cookies if necessary.
+                CookieEncoder cookieEncoder = new CookieEncoder(true);
+                for (Cookie cookie : cookies) {
+                    cookieEncoder.addCookie(cookie);
+                }
+            }
+        }
+        
+        if (sessionId != null) {
+        	//load the session.
+        	Map<String,Object> vals = this.getSessionPersistence(controller).loadSession(sessionId);
+        	if (vals != null) {
+        		controller.getSessionStorage().putAll(vals);
+        	}
+        	controller.getSessionStorage().put(SESSION, sessionId);
+        }
+
+	}
+
+	/**
+	 * returns the persistence provider, or defaultsessionpersistence if none is specified
+	 * @param controller
+	 * @return
+	 */
+	protected SessionPersistence getSessionPersistence(StrestController controller) {
+		String cls = controller.getServerConfig().getString("sessions.persistence", "com.trendrr.strest.contrib.sessions.DefaultSessionPersistence");
+		SessionPersistence persistence = this.persistence.get(cls);
+		if (persistence == null) {
+			try {
+				persistence = (SessionPersistence)Reflection.defaultInstance(cls);
+				this.persistence.put(cls, persistence);
+			} catch (Exception e) {
+				log.warn("Unable to load SessionPersistence class: " + cls);
+			}
+		}
+		if (persistence == null) {
+			return new DefaultSessionPersistence();
+		}
+		return persistence;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.trendrr.strest.server.StrestControllerFilter#after(com.trendrr.strest.server.StrestController)
+	 */
+	@Override
+	public void after(StrestController controller) throws StrestException {
+		if (controller.isStrest()) {
+			return;
+		}
+		
+		String sessionId = (String)controller.getSessionStorage().remove(SESSION);
+		if (sessionId == null && !controller.getSessionStorage().isEmpty()) {
+			CookieEncoder cookieEncoder = new CookieEncoder(true);
+			sessionId = UUID.randomUUID().toString();
+			Cookie cookie = new DefaultCookie(SESSION, sessionId);
+			cookie.setMaxAge(this.maxAge);
+			cookieEncoder.addCookie(cookie);
+	        controller.getResponse().setHeader(HttpHeaders.Names.COOKIE, cookieEncoder.encode());
+	        controller.getSessionStorage().put("expires", IsoDateUtil.getIsoDate(new Date(new Date().getTime()+(1000*this.maxAge))));
+		}
+		this.getSessionPersistence(controller).saveSession(sessionId, controller.getSessionStorage());
+	}
+
+	/* (non-Javadoc)
+	 * @see com.trendrr.strest.server.StrestControllerFilter#error(com.trendrr.strest.server.StrestController, org.jboss.netty.handler.codec.http.HttpResponse, java.lang.Exception)
+	 */
+	@Override
+	public void error(StrestController controller, HttpResponse response,
+			Exception exception) {
+		// TODO Auto-generated method stub
+
+	}
+}
