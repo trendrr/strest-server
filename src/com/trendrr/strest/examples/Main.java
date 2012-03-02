353b7da6816acb58eb426d20cd7e3a2d6b9dc45d
Dustin Norlander <dustinn@gmail.com>
2012-03-02 14:23:45 -0800

    use the builder

--- src/com/trendrr/strest/examples/Main.java
+++ src/com/trendrr/strest/examples/Main.java
@@ -6,6 +6,7 @@ package com.trendrr.strest.examples;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.trendrr.strest.StrestServerBuilder;
 import com.trendrr.strest.server.StrestServer;
 
 
@@ -19,7 +20,7 @@ public class Main {
 	protected static Log log = LogFactory.getLog(Main.class);
 	
 	public static void main(String[] args) throws Exception {
-		StrestServer server = StrestServer.instanceFromFile("example_config.yaml");
+		StrestServer server = new StrestServerBuilder().addConfigFile("example_config.yaml").build();
 		System.out.println("!!! WELCOME TO STREST !!!");
 		System.out.println("Strest-Server examples started");
 		server.start();
